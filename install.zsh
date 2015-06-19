edefad4d53c8836f78ee9b17ff23c1ab6d526e28
Alex Gray <alex@mrgray.com>
2015-06-19 11:00:57 -0400

    only conditionally generate key

--- install.zsh
+++ install.zsh
@@ -22,7 +22,7 @@ printf "\n\nWelcome to Ubuntu\n\nLet's make the z-shell + install git + make a /
 
 $INSTALL git zsh curl
 
-[[ -z "$KEYFILE" ]] && ssh-keygen -t rsa -b 4096 -C $GITMAIL -N "" -f 
+if [ ! -f "$KEYFILE" ]; then ssh-keygen -t rsa -b 4096 -C $GITMAIL -N "" -f "$KEYFILE"; fi
 
 # cat /dev/zero | ssh-keygen -q -t rsa -b 4096 -C $GITMAIL -N ""
 # ssh-keygen -y 
