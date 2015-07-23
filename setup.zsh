9f1dbe5d207d70db57fddcf9241e8183b9bdcdd2
Alex Gray <alex@mrgray.com>
2015-07-23 07:37:48 -0400

    wanting to try it out

--- setup.zsh
+++ setup.zsh
@@ -1,23 +1,22 @@
-#!/bin/sh
+#!/bin/zsh
 
 # bash <(wget -qO- https://raw.githubusercontent.com/mralexgray/ubuntu/master/setup.zsh)
 
-echo "what is the hostname of your new droplet? [$(hostname)]"
-read HOSTNAME
-HOSTNAME=$"{HOSTNAME:-$(hostname)}"
+setopt extendedglob
 
-echo "what is the ipV4 of your new droplet?"
-read IP
-echo "what is your dns.he.net login?"
-read LOGIN
-echo "what is your dns.he.net pass?"
-read PASS
+# HOSTNAME=$"{HOSTNAME:-$(hostname)}"
+# HOST=$(perl -e 'open IN, "</usr/share/dict/words";rand($.) < 1 && ($n=$_) while <IN>;print $n')
+# DOMAIN=$(domainname)
+HOSTNAME=cloud.$(domainname)
 
+read "HOSTNAME? What is the FQDN of your new droplet? [] ? "
+read 			 "IP?	What is the ipV4 of your new droplet? []  "
+read "DNS_USER? What is your dns.he.net login? [mralexgray]  "
+read "DNS_PASS? What is your dns.he.net pass? []  "
+           HOST="$(echo ${${HOSTNAME:-$RANDOM_NAME}/.*/})"
+				 DOMAIN="$(echo ${HOSTNAME/*./})"
 
-  HOST=${HOSTNAME/*./}
-DOMAIN=${HOSTNAME.*/}
-
-echo "setting up A record on $DOMAIN for $HOST with ip $IP for $LOGIN."
+echo "setting up A record on $DOMAIN for $HOST with ip $IP for ${DNS_USER:-mralexgray} on dns.he.net."
 
 cd /tmp
 git clone git@github.com:tresni/pyHurricaneDNS.git
