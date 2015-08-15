488802b3c673759695fdb02bad052673ce2d3652
Alex Gray <alex@mrgray.com>
2015-08-15 03:49:23 -0400

    making installable anywhere

--- setup.zsh
+++ setup.zsh
@@ -1,6 +1,20 @@
 #!/usr/bin/env zsh
 
+REPO=GoGoVPS
+
 # bash <(wget -qO- https://raw.githubusercontent.com/mralexgray/GoGoVPS/master/setup.zsh)
+
+# Make sure we're installed, otherwise do it.
+[[ $(basename $0) != $REPO  || ! $(git remote -b) =~ $REPO ]] && {
+	
+	echo "danger, not the right git repo!"
+	exit
+
+} || {
+
+	git submodule update --init --recursive
+	
+}
 
 # Set up DNS
 . ${0}/../setup/dns.zsh
